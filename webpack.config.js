ef3faff538a3f0550896fe8d7199593a251f7386
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-05-17 18:51:10 +0200

    Correct NPM script to support mode and env

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,10 @@ const webpackMerge = require('webpack-merge');
 /** @type {(env: any, argv: any) => webpack.Configuration} configuration */
 const configuration = (env = {}, argv) => {
   const common = require('./config/webpack.common')(env, argv);
-  if (env.production || env.prod) {
+  if (env.production || env.prod || argv.mode === 'production') {
     return webpackMerge(common, require('./config/webpack.prod')(env, argv));
   }
-  if (env.development || env.dev) {
+  if (env.development || env.dev || argv.mode === 'development') {
     return webpackMerge(common, require('./config/webpack.dev')(env, argv));
   }
 };

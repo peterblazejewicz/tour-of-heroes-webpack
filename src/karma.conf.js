edf2ce52ee851b71e08f593fbdf33867ec760bbf
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-05-19 23:59:48 +0200

    Karma config modification

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,14 +1,26 @@
-const { resolve } = require('path');
 module.exports = function(config) {
   config.set({
     basePath: '..',
     frameworks: ['jasmine', 'karma-typescript'],
-
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '../coverage/TourOfHeroes'),
+      reports: ['html'],
+      skipFilesWithNoCoverage: true,
+    },
     files: [
       { pattern: 'src/base.spec.ts' },
       { pattern: 'src/app/**/*.+(ts|html)' },
     ],
-
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-typescript'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+    ],
     preprocessors: {
       '**/*.ts': ['karma-typescript'],
     },
@@ -22,7 +34,6 @@ module.exports = function(config) {
         lib: ['ES2015', 'DOM'],
       },
     },
-
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
